84902a579d4110b055f543cf9424b85c99c3d856
Adrian Grzelak <grzadr@gmail.com>
2017-12-08 02:46:51 +0100

    Visualizer seems fine - however untested yet

--- fifteen/visualize/src/Window15.java
+++ fifteen/visualize/src/Window15.java
@@ -1,12 +1,92 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import java.awt.Font;
+import java.util.Hashtable;
+import java.util.List;
 
 
-public class Window15 extends JFrame {
+public class Window15 extends JFrame implements ChangeListener {
 
 	private static final long serialVersionUID = 1L;
 	
-	public Window15() {
-
+	private List<List<Integer>> list;
+	private int rows;
+	private int cols;
+	
+	private static int BOUND=10;
+	private static int DISTANCE=4; //Between Tiles
+	private static int TILE_SIZE=80;
+	private static int SLIDER_HEIGHT=50;
+	
+	private JButton[] puzzle;
+	private JSlider slider;
+	
+	
+	public Window15(Integer rows, Integer cols, List<List<Integer>> list, String moveSeq) {
+		this.list = list;
+		this.rows = rows;
+		this.cols = cols;
+		
+		int windowWidth = 2*BOUND + cols*TILE_SIZE + cols*DISTANCE+14;
+		int windowHeight = 2*BOUND + rows*TILE_SIZE + rows*DISTANCE + SLIDER_HEIGHT + 50;
+		setSize(windowWidth, windowHeight);
+		setTitle("Solving " + rows + "x" + cols + " puzzle");
+		setLayout(null);
+		Integer size = rows*cols;
+		
+		puzzle = new JButton[size];
+		
+		//initialize puzzle with 0 state
+		for (int i=0; i<size ; i++) {
+			puzzle[i] = new JButton(Integer.toString(list.get(0).get(i)));
+			Integer currentRow = i/cols;
+			Integer currentCol = i%cols;
+			System.err.println(currentRow +" "+ currentCol);
+			puzzle[i].setBounds(BOUND + currentCol*(DISTANCE+TILE_SIZE), BOUND + currentRow*(DISTANCE+TILE_SIZE), TILE_SIZE, TILE_SIZE);
+			puzzle[i].setFont(new Font("Arial", Font.BOLD, 40));
+			puzzle[i].setEnabled(false);
+			add(puzzle[i]);
+			if(puzzle[i].getText().equals("0")) puzzle[i].setVisible(false);
+			puzzle[i].setEnabled(false);
+		}
+		
+		slider = new JSlider(JSlider.HORIZONTAL, 0, list.size()-1, 0);
+	    slider.setBounds(BOUND-3, 2*BOUND+rows*TILE_SIZE+rows*DISTANCE, windowWidth-2*BOUND, SLIDER_HEIGHT);
+	    add(slider);
+	    //Turn on labels at major tick marks.
+	    slider.setMajorTickSpacing(list.size());
+	    slider.setMinorTickSpacing(list.size()/10);
+	    slider.setPaintTicks(true);
+	    slider.setPaintLabels(true);
+	    //Create the label table
+	    Hashtable<Integer, JLabel> labelTable = new Hashtable<Integer, JLabel>();
+	    labelTable.put( new Integer( 0 ), new JLabel("Start State") );
+	    labelTable.put( new Integer( list.size()-1 ), new JLabel("Solved") );
+	    slider.setLabelTable( labelTable );
+		
+	    slider.addChangeListener(this);
+	   
+	}
+	
+	@Override
+	public void stateChanged(ChangeEvent e) {
+	    JSlider source = (JSlider)e.getSource();
+	    if (!source.getValueIsAdjusting()) {
+	        int state = (int)source.getValue();
+	        
+			for (int i=0; i<rows*cols ; i++) {
+				puzzle[i].setText(Integer.toString(list.get(state).get(i)));
+				if(puzzle[i].getText().equals("0")) puzzle[i].setVisible(false);
+				else puzzle[i].setVisible(true);
+			}
+	        
+	    }
 	}
 	
 }
--- fifteen/visualize/src/gui.java
+++ fifteen/visualize/src/gui.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -6,45 +7,106 @@ public class gui {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
+	
+	
+	public static void main(String[] args) throws Exception {
 		//args: rows cols array[rows*cols] string
 		
 		Integer rows;
 		Integer cols;
 		List<List<Integer>> list = new ArrayList<List<Integer>>();
-		String moveSeq;
+		int solutionLength;
+		String moveSeq = "";
 	
 		//Getting values from args
 		try {
 			rows = Integer.parseInt(args[0]);
 			cols = Integer.parseInt(args[1]);
 			List<Integer> first = new ArrayList<Integer>();
+			
 			for (int i=2;i<rows*cols+2;i++) {
 				first.add(Integer.parseInt(args[i]));
 			}
 			list.add(first);
-			moveSeq = args[rows*cols+2];
+			
+			solutionLength = Integer.parseInt(args[rows*cols+2]);
+			if (solutionLength > 0) moveSeq = args[rows*cols+3];
+				
+			//Makeing list of moves
+			for (int i=0; i<moveSeq.length(); i++) {
+				List<Integer> next = new ArrayList<Integer>();
+				next = moveFreeTile(rows,cols,list.get(i),moveSeq.charAt(i));
+				list.add(next);
+			}
 		} catch (Exception ex) {
-			System.err.println("Bad format of args");
+			System.err.println("Bad format of args" + ex.getMessage());
 			return;
 		}
 		
-		System.out.println(rows);
-		System.out.println(cols);
-		System.out.println(list.get(0));
-		System.out.println(moveSeq);
+		System.err.println(rows);
+		System.err.println(cols);
+		for(int i=0;i<list.size();i++) {
+			System.err.println(i + ". " + list.get(i));
+		}
+		System.err.println(moveSeq);
 		
-		//Makeing list of
-
-		
-		
-		
-		
-		Window15 okno = new Window15();
+		Window15 okno = new Window15(rows,cols,list,moveSeq);
 		okno.setDefaultCloseOperation(Window15.DISPOSE_ON_CLOSE);
 		okno.setVisible(true);
 
+	}
+	
+	static List<Integer> moveFreeTile(Integer rows, Integer cols, List<Integer> val, char moveDirection) throws Exception
+	{
+		List<Integer> values = new ArrayList<Integer>(val);
+		//Getting position of 0 element
+		int zeroPosition = -1;
+		for (int i = 0; i < values.size(); i++)
+		{
+			if (values.get(i) == 0)
+			{
+				zeroPosition = i;
+				break;
+			}
+		}
+
+		if (zeroPosition == -1) throw new Exception("Board doesn't have 0 element?!");
+
+		if (moveDirection == 'L') {
+			//We can't left if 0 element is in first column
+			if (zeroPosition%cols == 0) throw new Exception("Illegal Move");
+			else {
+				Collections.swap(values,zeroPosition, zeroPosition-1);
+			}
+		}
+		else if (moveDirection == 'P') {
+			//We can't right if 0 element is in last column
+			if ((zeroPosition+1)%cols == 0) throw new Exception("Illegal Move");
+			else {
+				Collections.swap(values,zeroPosition, zeroPosition +1);
+				return values;
+			}
+		}
+		else if (moveDirection == 'G') {
+			//We can't up if 0 element is in first row
+			if (zeroPosition/cols == 0) throw new Exception("Illegal Move");
+			else {
+				Collections.swap(values,zeroPosition, zeroPosition - cols);
+				return values;
+			}
+		}
+		else if (moveDirection == 'D') {
+			//We can't down if 0 element is in last row
+			if (zeroPosition / cols == rows-1) throw new Exception("Illegal Move");
+			else {
+				Collections.swap(values,zeroPosition, zeroPosition + cols);
+				return values;
+			}
+		}
+		else throw new Exception("Illegal Move");
+		return values;
 	}
 
 }
